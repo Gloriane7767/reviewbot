63e73e9052cf5f615a1886a3def5d2ca34c5c78b
GLORY NGUM FONCHA <gloriane7767@gmail.com>
2026-05-08 21:48:30 +0200

    Initial commit

---
+++ src/test/java/com/gloriane/reviewbot/ReviewbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gloriane.reviewbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
